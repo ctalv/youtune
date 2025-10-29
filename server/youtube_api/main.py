26d92376e48190ff3eb19455b112a9143987d2f5
ctalv <clairetalverson@gmail.com>
2025-10-28 22:55:13 -0500

    YouTube api working

--- server/youtube_api/main.py
+++ server/youtube_api/main.py
@@ -1,16 +1,11 @@
 # -*- coding: utf-8 -*-
 
-# Sample Python code for youtube.channels.list
+# Sample Python code for youtube.search.list
 # See instructions for running these code samples locally:
 # https://developers.google.com/explorer-help/code-samples#python
 
 import os
-
-import google_auth_oauthlib.flow
 import googleapiclient.discovery
-import googleapiclient.errors
-
-scopes = ["https://www.googleapis.com/auth/youtube.readonly"]
 
 def main():
     # Disable OAuthlib's HTTPS verification when running locally.
@@ -19,17 +14,13 @@ def main():
 
     api_service_name = "youtube"
     api_version = "v3"
-    client_secrets_file = "YOUR_CLIENT_SECRET_FILE.json"
+    DEVELOPER_KEY = os.environ["API_KEY"]
 
-    # Get credentials and create an API client
-    flow = google_auth_oauthlib.flow.InstalledAppFlow.from_client_secrets_file(
-        client_secrets_file, scopes)
-    credentials = flow.run_console()
     youtube = googleapiclient.discovery.build(
-        api_service_name, api_version, credentials=credentials)
+        api_service_name, api_version, developerKey = DEVELOPER_KEY)
 
-    request = youtube.channels().list(
-        
+    request = youtube.search().list(
+        part="snippet"
     )
     response = request.execute()
 
